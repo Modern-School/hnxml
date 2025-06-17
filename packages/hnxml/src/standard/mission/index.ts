5510e4b3678d79128f7a7baf0783a267bb6a2d38
BovineBeta <bovinefbik@foxmail.com>
2025-06-18 00:32:35 +0800

    fix: mission's children allow single

---
+++ packages/hnxml/src/standard/mission/index.ts
@@ -0,0 +1,40 @@
+import type { HXGoal, HXGoals } from "./goals.ts";
+import { render } from "jsx-xml";
+import type { HXMissionEmail } from "./email.ts";
+
+export namespace Mission {
+  export interface start {
+    val: string;
+    suppress: "true" | "false";
+    children: string;
+  }
+  export interface end {
+    val: string;
+    children: string;
+  }
+  export interface next {
+    IsSilent: "true" | "false";
+    children: string;
+  }
+  export interface branchs {
+    children: void | branch | branch[];
+  }
+  export interface branch {
+    children: string;
+  }
+}
+
+type MissionChildren =
+  | HXGoals
+  | HXMissionEmail
+  | Mission.start
+  | Mission.end
+  | Mission.next
+  | Mission.branchs;
+
+export interface HXMission {
+  id: string;
+  activeCheck?: "true" | "false";
+  shouldIgnoreSenderVerification?: "true" | "false";
+  children: void | MissionChildren | MissionChildren[];
+}

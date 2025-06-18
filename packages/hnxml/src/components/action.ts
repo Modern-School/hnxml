e8fae49bb34e256f94a4a45f0be58c11ba679700
BovineBeta <bovinefbik@foxmail.com>
2025-06-18 21:35:59 +0800

    feat: divide action into action and task
    把action中的task拆分出来，用于顺便给faction用

--- packages/hnxml/src/components/action.ts
+++ packages/hnxml/src/components/action.ts
@@ -1,5 +1,5 @@
 import type { HXConditionalActions } from "../standard/action/index.ts";
-import { Task } from "../standard/action/task.ts";
+
 import { Trigger } from "../standard/action/trigger.ts";
 
 declare global {
@@ -11,32 +11,6 @@ declare global {
       OnAdminGained: Trigger.OnAdminGained;
       Instantly: Trigger.Instantly;
       OnDisconnect: Trigger.OnDisconnect;
-      RunFunction: Task.RunFunction;
-      LoadMission: Task.LoadMission;
-      AddAsset: Task.AddAsset;
-      CopyAsset: Task.CopyAsset;
-      AddMissionToHubServer: Task.AddMissionToHubServer;
-      RemoveMissionFromHubServer: Task.RemoveMissionFromHubServer;
-      AddThreadToMissionBoard: Task.AddThreadToMissionBoard;
-      AddIRCMessage: Task.AddIRCMessage;
-      CrashComputer: Task.CrashComputer;
-      DeleteFile: Task.DeleteFile;
-      AddConditionalActions: Task.AddConditionalActions;
-      SaveGame: Task.SaveGame;
-      LaunchHackScript: Task.LaunchHackScript;
-      SwitchToTheme: Task.SwitchToTheme;
-      StartScreenBleedEffect: Task.StartScreenBleedEffect;
-      CancelScreenBleedEffect: Task.CancelScreenBleedEffect;
-      AppendToFile: Task.AppendToFile;
-      KillExe: Task.KillExe;
-      HideNode: Task.HideNode;
-      GivePlayerUserAccount: Task.GivePlayerUserAccount;
-      ChangeIP: Task.ChangeIP;
-      HideAllNodes: Task.HideAllNodes;
-      ShowNode: Task.ShowNode;
-      SetLock: Task.SetLock;
-      ChangeAlertIcon: Task.ChangeAlertIcon;
-      ChangeNetmapSortMethod: Task.ChangeNetmapSortMethod;
     }
   }
 }

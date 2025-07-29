9cd77cb6504b16da4d2e101e38d611a041cc6097
BovineBeta <bovinefbik@foxmail.com>
2025-07-29 12:44:57 +0800

    feat: export JSX namespace
    默认导出JSX命名空间，现在不需要在使用hnxml的项目declare global了

--- jsx-runtime.ts
+++ jsx-runtime.ts
@@ -4,3 +4,22 @@ export type {
   JsxXmlElement,
   JsxXmlTagElement,
 } from "jsx-xml/jsx-runtime";
+
+import { TriggerElements } from "./src/components/action.ts";
+import { ComputerElements } from "./src/components/computer.ts";
+import { FactionElements } from "./src/components/faction.ts";
+import { InfoElements } from "./src/components/info.ts";
+import { MissionElements } from "./src/components/mission.ts";
+import { TaskElements } from "./src/components/task.ts";
+/**
+ * the JSX namespace for using hnxml with JSX/TSX
+ */
+export namespace JSX {
+  export type IntrinsicElements =
+    & TriggerElements
+    & ComputerElements
+    & FactionElements
+    & InfoElements
+    & MissionElements
+    & TaskElements;
+}

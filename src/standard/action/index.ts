c91fb3889407b59e3978ce3db588f879fd6f0645
FBIKdot <FBIKdot@users.noreply.github.com>
2025-08-28 16:28:00 +0800

    docs(action): main lang: en -> cn

--- src/standard/action/index.ts
+++ src/standard/action/index.ts
@@ -2,10 +2,7 @@ import { AllTriggers } from "./trigger.ts";
 import { Trigger } from "./trigger.ts";
 
 /**
- * `<ConditionalActions>` is the root element of Actions.
- * Child Elements of `<ConditionalActions>` are all types of Trigger
- *
- * @localeCN Action 可以根据特定条件来修改、控制游戏内容。检测条件的东西叫做 Trigger (触发器)。
+ * Action 可以根据特定条件来修改、控制游戏内容。检测条件的东西叫做 Trigger (触发器)。
  * 当条件满足后，Trigger 会被触发，触发 Trigger 后干的事情叫做 Task (行为，……其实更应该叫做任务，但是任务和Mission冲突了)
  *
  * 为了减少歧义，请尽量不使用译文。
@@ -14,13 +11,15 @@ import { Trigger } from "./trigger.ts";
  * 见：[Hacknet-Extension-Tutorial/Actions](https://github.com/FBIKdot/Hacknet-Extension-Tutorial/blob/main/Content/Actions.md)
  *
  * `<ConditionalActions>` 是 Actions 的根元素。 `<ConditionalActions>` 的子元素是 Trigger。
+ * @localeEnglish `<ConditionalActions>` is the root element of Actions.
+ * Child Elements of `<ConditionalActions>` are all types of Trigger
+ *
  * @see {@link Trigger}, {@link AllTriggers}
  */
 export interface ConditionalActionsElement {
   /**
-   * Child Elements of `<ConditionalActions>` are all types of Trigger
-   *
-   * @localeCN `<ConditionalActions>` 的子元素是所有种类的 Trigger
+   * `<ConditionalActions>` 的子元素是所有种类的 Trigger
+   * @localeEnglish Child Elements of `<ConditionalActions>` are all types of Trigger
    * @see {@link Trigger}, {@link AllTriggers}
    */
   children: " " | AllTriggers | AllTriggers[];
